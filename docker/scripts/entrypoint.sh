a797d40daf51a0f4788294595fc940489320957d
cguerra <carlos.guerra.lopez@datiobbva.com>
2021-05-30 14:13:03 +0200

    Adapt dockerfile for configure path, modify ingress for accept websocket, adapt deployment for use last version of docker image

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git clone $REPOSITORY repo
 pip install -r repo/requirements.txt
-streamlit run repo/app.py --global.logLevel=debug --server.baseUrlPath=$APP_PATH
+streamlit run repo/app.py --server.baseUrlPath=$APP_PATH
